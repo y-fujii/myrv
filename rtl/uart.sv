9bf2ea35147b32514c73c892c5318fd877910ceb
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2024-10-06 22:40:29 +0900

    improve uart.

--- rtl/uart.sv
+++ rtl/uart.sv
@@ -7,30 +7,24 @@ module UartTx#(parameter N_CYCLES)(
 	output logic      ready,
 	output logic      tx
 );
-	logic[11:0] n_clks;
-	logic[ 3:0] n_bits;
+	(* onehot *)
+	logic[10:0] state;
+	logic[$clog2(N_CYCLES)-1:0] count;
 
-	always_comb ready = n_bits == 4'b1000;
-
-	always_comb unique casez (n_bits)
-		4'b1111 : tx = 1'b0;
-		4'b0??? : tx = data[n_bits[2:0]];
-		default : tx = 1'b1;
-	endcase
+	always_comb ready = state[10];
+	always_comb tx = |(state & {1'b1, data, 2'b01});
 
 	always_ff @(posedge clock) begin
-		if (reset)
-			n_clks <= 0;
-		else if (n_clks == N_CYCLES - 1)
-			n_clks <= 0;
+		if (reset | ~|count)
+			count <= N_CYCLES - 1;
 		else
-			n_clks <= n_clks + 12'b1;
+			count <= count - 1'b1;
 
 		if (reset)
-			n_bits <= 4'b1000;
+			state <= 11'b1_0000_0000_00;
 		else if (valid & ready)
-			n_bits <= 4'b1110;
-		else if (n_clks == N_CYCLES - 1 & !ready)
-			n_bits <= n_bits + 4'b1;
+			state <= 11'b0_0000_0000_01;
+		else if (~|count)
+			state <= {|state[10:9], state[8:0], 1'b0};
 	end
 endmodule
