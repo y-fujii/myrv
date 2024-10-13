f6c297611b3d9956b4e26d5c37b8b1b14dceab98
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2024-10-13 18:36:51 +0900

    improve uart.

--- rtl/uart.sv
+++ rtl/uart.sv
@@ -7,24 +7,23 @@ module UartTx#(parameter N_CYCLES)(
 	output logic      ready,
 	output logic      tx
 );
-	(* onehot *)
-	logic[10:0] state;
+	logic[                10:0] state;
 	logic[$clog2(N_CYCLES)-1:0] count;
 
-	always_comb ready = state[10];
-	always_comb tx = reset | |(state & {1'b1, data, 2'b01});
+	always_comb ready = ~reset & state == 11'b1;
+	always_comb tx    =  reset | state[0];
 
 	always_ff @(posedge clock) begin
 		if (reset | ~|count)
 			count <= $size(count)'(N_CYCLES - 1);
 		else
-			count <= count - 1'b1;
+			count <= count - 'b1;
 
 		if (reset)
-			state <= 11'b1_0000_0000_00;
+			state <= '1;
 		else if (valid & ready)
-			state <= 11'b0_0000_0000_01;
-		else if (~|count)
-			state <= {|state[10:9], state[8:0], 1'b0};
+			state <= {1'b1, data, 2'b01};
+		else if (~|count & !ready)
+			state <= state >> 1;
 	end
 endmodule
